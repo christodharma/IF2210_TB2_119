4be9ed6c4aaa93dbecec855a1feecdd818abb322
christodharma <13521009@std.stei.itb.ac.id>
2023-04-28 16:45:27 +0700

    feat: add product method, test directories

--- src/main/java/Products/Product.java
+++ src/main/java/Products/Product.java
@@ -4,7 +4,55 @@ import java.io.Serializable;
 
 public class Product implements Serializable {
     private static final long serialVersionUID = System.currentTimeMillis();
-//    potential attributes: quantity
-    protected String ID;
-    protected double Price;
+    private String ID;
+    private String Name;
+    private double Price;
+    private int StockQuantity;
+    public Product(String newID, String newName, double newPrice, int newStockQuantity)
+    {
+        this.ID = newID;
+        this.Name = newName;
+        this.Price = newPrice;
+        this.StockQuantity = newStockQuantity;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getName(){
+        return Name;
+    }
+
+    public double getPrice() {
+        return Price;
+    }
+
+    public int getStockQuantity() {
+        return StockQuantity;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public void setPrice(double price) {
+        Price = price;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        StockQuantity = stockQuantity;
+    }
+    public void addStockQuantity(int many){
+        StockQuantity+=many;
+    }
+    public void subtractStockQuantity(int many){
+        if (StockQuantity>=many){
+            StockQuantity-=many;
+        }
+    }
 }
