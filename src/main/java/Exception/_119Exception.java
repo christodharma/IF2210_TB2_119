8ccb46a93ebec97ce948b0d41a9eeb316aaa9863
christodharma <13521009@std.stei.itb.ac.id>
2023-05-04 14:41:21 +0700

    feat : _119Exception package for exceptions by _119

---
+++ src/main/java/Exception/_119Exception.java
@@ -0,0 +1,18 @@
+package Exception;
+
+public class _119Exception extends Exception{
+    public _119Exception(){
+        super();
+    }
+    public _119Exception(String message){
+        super (message);
+    }
+    public _119Exception(String message, Throwable cause)
+    {
+        super (message,cause);
+    }
+    public _119Exception(Throwable cause)
+    {
+        super(cause);
+    }
+}
