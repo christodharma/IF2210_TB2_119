67c04719eb38372e5670af4c0f8ebb5b4f96df0f
Muhammad Malik Ibrahim Baharsyah <88836015+malikbaharsyah@users.noreply.github.com>
2023-05-08 01:10:45 +0700

    add Plugin (70%)
    Co-Authored-By: Agsha Athalla Nurkareem <110584940+agshaathalla@users.noreply.github.com>

---
+++ src/main/java/BasePlugin/BlankPage.java
@@ -0,0 +1,45 @@
+package BasePlugin;
+
+import javax.swing.*;
+import java.awt.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BlankPage extends JFrame {
+    private String title;
+    private JPanel panel;
+
+    public BlankPage() {
+        // Set up the frame properties
+        setTitle("Blank Page");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 400);
+        
+        // Create a container panel
+        JPanel panel = new JPanel();
+        getContentPane().add(panel);
+        
+        // Set the layout of the container panel
+        panel.setLayout(new GridLayout(1, 1));
+        this.panel = panel;
+    }
+
+    public void setGrid(int rows, int columns){
+        this.panel.setLayout(new GridLayout(rows, columns));
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    
+
+    // public static void main(String[] args) {
+    //     // Create an instance of your blank page
+    //     BlankPage page = new BlankPage();
+    //     page.setVisible(true);
+    // }
+}
