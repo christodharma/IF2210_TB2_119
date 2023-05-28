30e95e9c00a0be06d0ab96c6d25951576decaf41
christodharma <13521009@std.stei.itb.ac.id>
2023-05-28 13:44:58 +0700

    fix: fixedbill deserialize

--- src/main/java/Model/Transactions/FixedBill.java
+++ src/main/java/Model/Transactions/FixedBill.java
@@ -1,8 +1,10 @@
 package Model.Transactions;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -13,8 +15,8 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
-@RequiredArgsConstructor
 @Data
+@AllArgsConstructor
 public class FixedBill implements Serializable {
     private static final long serialVersionUID = 3L;
     @JsonProperty("buyerID")
@@ -22,12 +24,27 @@ public class FixedBill implements Serializable {
     @JsonProperty("items")
     private final ArrayList<FixedBillEntry> items;
     @JsonProperty("datetime") @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss.SSSZ")
-    private final ZonedDateTime dateTime = ZonedDateTime.now();
+    private final ZonedDateTime dateTime;
     @JsonIgnore
     public String getID() {
         DateTimeFormatter CUSTOM_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
         LocalDateTime ldt = LocalDateTime.now();
         String formatteddatetime = ldt.format(CUSTOM_FORMATTER);
         return buyerID + formatteddatetime;
+    }
+
+    public FixedBill(String buyerID, ArrayList<FixedBillEntry> items) {
+        this.buyerID = buyerID;
+        this.items = items;
+        this.dateTime = ZonedDateTime.now();
+    }
+
+    @JsonCreator
+    public FixedBill JacksonCreate(
+            @JsonProperty("buyerID") String id,
+            @JsonProperty("items") ArrayList<FixedBillEntry> items,
+            @JsonProperty("datetime") ZonedDateTime dt
+    ){
+        return new FixedBill(id, items, dt);
     }
 }
